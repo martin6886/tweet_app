8bb2f146d1118b855c50f3652d601a233ce430df
martin6886 <ib230004hiep@setouchicollege.com>
2024-11-23 13:04:57 +0900

    p241完了

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -13,4 +13,9 @@ class Tweet extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function images()
+    {
+        return $this->belongsTomany(Image::class, 'tweet_images')
+        ->using(TweetImage::class);
+    }
 }
